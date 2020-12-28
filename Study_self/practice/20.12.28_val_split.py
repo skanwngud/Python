e748245501a9c5421e7df5da23943e696c118880
skanwngud <skanwngud@gmail.com>
2020-12-28 10:46:43 +0900

    commit

--- Study_self/practice/20.12.28_val_split.py
+++ Study_self/practice/20.12.28_val_split.py
@@ -16,23 +16,29 @@ y_train=y[:80]
 y_test=y[80:]
 
 model=Sequential()
-model.add(Dense(20, input_dim=1, activation='relu'))
-model.add(Dense(20))
+model.add(Dense(50, input_dim=1, activation='relu'))
+model.add(Dense(50))
+model.add(Dense(50))
+model.add(Dense(50))
+model.add(Dense(50))
+model.add(Dense(50))
+model.add(Dense(50))
+model.add(Dense(50))
 model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam', metrics=['mae'])
 model.fit(x_train, y_train,epochs=100, batch_size=1, validation_split=0.2)
 
 results=model.evaluate(x_test, y_test, batch_size=1)
-print(results)
+print('mse, mae : ', results)
 
 predict=model.predict(y_test)
-print(predict)
+print('y_test : ', predict)
 
 def RMSE(y_test, predict):
     return np.sqrt(mean_squared_error(y_test, predict))
 
-print(RMSE(y_test, predict))
+print('rmse : ', RMSE(y_test, predict))
 
 r2=r2_score(y_test, predict)
-print(r2)
+print('r2:', r2)
