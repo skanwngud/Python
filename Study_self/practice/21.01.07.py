e26eeb0ae44983c65d0008bf74fa9c498036c343
skanwngud <skanwngud@gmail.com>
2021-01-07 21:09:19 +0900

    commit

--- Study_self/practice/21.01.07.py
+++ Study_self/practice/21.01.07.py
@@ -1,2 +1,34 @@
 import numpy as np
 
+from sklearn.preprocessing import OneHotEncoder
+
+from tensorflow.keras.utils import to_categorical
+
+a=np.array([3,6,5,4,2])
+
+# a=a.reshape(1, -1)
+# enc=OneHotEncoder()
+# enc.fit(a)
+# a=enc.transform(a).toarray()
+
+# print(a)
+# print(a.shape)
+
+# # [[0. 1. 0. 0. 0.]
+# #  [0. 0. 0. 0. 1.]
+# #  [0. 0. 0. 1. 0.]
+# #  [0. 0. 1. 0. 0.]
+# #  [1. 0. 0. 0. 0.]]
+# # (5, 5)
+
+a=to_categorical(a)
+
+print(a)
+print(a.shape)
+
+# [[0. 0. 0. 1. 0. 0. 0.]
+#  [0. 0. 0. 0. 0. 0. 1.]
+#  [0. 0. 0. 0. 0. 1. 0.]
+#  [0. 0. 0. 0. 1. 0. 0.]
+#  [0. 0. 1. 0. 0. 0. 0.]]
+# (5, 7)
