57cca2537372b7b4025be90b1ee521e109eec3cb
skanwngud <skanwngud@gmail.com>
2021-01-11 19:58:31 +0900

    commit

--- Study_self/practice/21.01.11_matplotlib_font.py
+++ Study_self/practice/21.01.11_matplotlib_font.py
@@ -1,89 +1,64 @@
-random_seed=66
-
 import numpy as np
 import matplotlib.pyplot as plt
 
-from tensorflow.keras.datasets import mnist
-
-(x_train, y_train), (x_test, y_test)=mnist.load_data()
-
-# print(x_train.shape, y_train.shape) # (60000, 28, 28) (60000,)
-# print(x_test.shape, y_test.shape) # (10000, 28, 28) (10000,)
-
-# print(x_train[0])
-# print(y_train[0])
-# print(x_train[0].shape)
-
-x_train=x_train.reshape(x_train.shape[0], x_train.shape[1], x_train.shape[2], 1).astype('float32')/255. # dtype 도 float 로 바꿈
-x_test=x_test.reshape(x_test.shape[0], x_test.shape[1], x_test.shape[2], 1)/255.
-
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, Input, Dropout, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.callbacks import EarlyStopping, TensorBoard, ModelCheckpoint
 from tensorflow.keras.utils import to_categorical
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten, Dropout
-from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
+
+from sklearn.model_selection import train_test_split
+
+(x_train, y_train), (x_test, y_test)=cifar10.load_data()
+x_train, x_val, y_train, y_val=train_test_split(x_train, y_train, train_size=0.8, random_state=56)
+
+x_train=x_train/255.
+x_test=x_test/255.
+x_val=x_val/255.
 
 y_train=to_categorical(y_train)
 y_test=to_categorical(y_test)
+y_val=to_categorical(y_val)
 
-model=Sequential()
-model.add(Conv2D(filters=128, kernel_size=(3,3), padding='same', strides=1, input_shape=(28,28,1)))
-model.add(MaxPooling2D(pool_size=2))
-model.add(Dropout(0.25))
-model.add(Conv2D(128, kernel_size=(3,3), padding='same'))
-model.add(MaxPooling2D(pool_size=2))
-model.add(Dropout(0.25))
-model.add(Conv2D(128, 3, padding='same'))
-model.add(MaxPooling2D(pool_size=2))
-model.add(Dropout(0.25))
-model.add(Flatten())
-model.add(Dense(150, activation='relu'))
-model.add(Dense(200, activation='relu'))
-model.add(Dense(250, activation='relu'))
-model.add(Dense(300, activation='relu'))
-model.add(Dense(400, activation='relu'))
-model.add(Dense(300, activation='relu'))
-model.add(Dense(250, activation='relu'))
-model.add(Dense(200, activation='relu'))
-model.add(Dense(150, activation='relu'))
-model.add(Dense(100, activation='relu'))
-model.add(Dense(80, activation='relu'))
-model.add(Dense(30, activation='relu'))
-model.add(Dense(10, activation='softmax'))
+input=Input(shape=(x_train.shape[1], x_train.shape[2], x_train.shape[3]))
+cnn1=Conv2D(100, (2,2), padding='same', activation='relu')(input)
+max1=MaxPooling2D((2,2))(cnn1)
+drop1=Dropout(0.2)(max1)
+cnn1=Conv2D(150, (2,2), padding='same', activation='relu')(drop1)
+cnn1=Conv2D(200, (2,2), padding='same', activation='relu')(cnn1)
+max1=MaxPooling2D((2,2))(cnn1)
+drop1=Dropout(0.2)(max1)
+cnn1=Conv2D(250, (2,2), padding='same', activation='relu')(drop1)
+max1=MaxPooling2D((2,2))(cnn1)
+drop1=Dropout(0.2)(max1)
+flat1=Flatten()(drop1)
+dense1=Dense(150, activation='relu')(flat1)
+dense1=Dense(200, activation='relu')(dense1)
+dense1=Dense(150, activation='relu')(dense1)
+dense1=Dense(100, activation='relu')(dense1)
+dense1=Dense(80, activation='relu')(dense1)
+dense1=Dense(50, activation='relu')(dense1)
+output=Dense(10, activation='softmax')(dense1)
+model=Model(input, output)
 
-cp=ModelCheckpoint(filepath='./skanwngud/Study/modelCheckpoint/assign_{epoch:02d}-{val_loss:.4f}.hdf5')
-early=EarlyStopping(monitor='loss', patience=5, mode='auto')
+es=EarlyStopping(monitor='val_loss', patience=10, mode='auto')
+cp=ModelCheckpoint(filepath='./skanwngud/Study/modelCheckpoint/review_1_{epoch:02d}-{val_loss:.5f}.hdf5',
+                    save_best_only=True, monitor='val_loss', mode='auto')
+tb=TensorBoard(log_dir='./skanwngud/Study/graph', write_graph=True, write_images=True, histogram_freq=0)
+
 model.compile(loss='categorical_crossentropy', optimizer='adam', metrics='acc')
-hist=model.fit(x_train, y_train, epochs=5, batch_size=64, validation_split=0.2, callbacks=early)
+model.fit(x_train, y_train, epochs=50, validation_data=(x_val, y_val), callbacks=[es, tb, cp])
 
 loss=model.evaluate(x_test, y_test)
-y_pred=model.predict(x_test[:10])
+y_pred=model.predict(x_test)
 
-print(loss)
-print(y_pred)
-print(y_test[:10])
-print(np.argmax(y_test[:10], axis=-1))
+print('loss : ', loss[0])
+print('acc : ', loss[1])
+print('y_test : ', np.argmax(y_test[:5], axis=-1))
+print('y_pred : ', np.argmax(y_pred[:5], axis=-1))
 
-plt.figure(figsize=(10, 6)) # (10, 6) 의 면적을 잡음
-
-plt.rc('font', family='Malgun Gothic')
-plt.subplot(2,1,1) # (2, 1) 짜리 그림을 만듬 (2행 1열의 그림 중 '첫 번째')
-plt.plot(hist.history['loss'], marker='.', c='red', label='loss')
-plt.plot(hist.history['val_loss'], marker='.', c='blue', label='val_loss')
-plt.grid() # 격자의 형태
-
-plt.title('손실비용')
-plt.ylabel('loss')
-plt.xlabel('epoch')
-plt.legend(loc='upper right')
-
-plt.subplot(2,1,2) # (2, 1) 짜리 그림을 만듬 (2행 1열의 그림 중 '두 번째')
-plt.plot(hist.history['acc'], marker='.', c='red', label='acc') # model.compile 에서 accuracy 로 썼으면 여기서도 풀네임으로 써야한다
-plt.plot(hist.history['val_acc'], marker='.', c='blue', label='val_acc')
-plt.grid()
-
-plt.title('정확도')
-plt.ylabel('acc')
-plt.xlabel('epoch')
-plt.legend(loc='upper right')
-
-plt.show()
+# results
+# loss :  0.8146396279335022
+# acc :  0.7434999942779541
+# y_test :  [3 8 8 0 6]
+# y_pred :  [3 8 8 0 6]
