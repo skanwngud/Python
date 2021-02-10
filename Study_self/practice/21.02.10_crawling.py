711dde7d33579b82de92c2896ccf11cb595aa53d
skanwngud <skanwngud@gmail.com>
2021-02-10 18:44:57 +0900

    commit

--- Study_self/practice/21.02.10_crawling.py
+++ Study_self/practice/21.02.10_crawling.py
@@ -1,15 +1,15 @@
 from bs4 import BeautifulSoup as bs
 from urllib.request import urlopen
 from urllib.parse import quote_plus
-'''
-baseUrl="https://search.naver.com/search.naver?where=nexearch&sm=top_hty&fbm=0&ie=utf8&query="
+
+baseUrl="https://pixabay.com/ko/photos/search/"
 plusUrl=input('검색어 :')
 crawl_num=int(input('크롤링 할 갯수 : '))
 
-url=baseUrl+quote_plus(plusUrl)
+url=baseUrl+quote_plus(plusUrl+'/')
 html=urlopen(url)
 soup=bs(html, 'html.parser')
-img=soup.find_all(class_='<img>')
+img=soup.find_all(class_='no_header_seach')
 
 n=1
 for i in img:
@@ -24,4 +24,3 @@ for i in img:
         break
 
 print('이미지 크롤링 종료')
-'''
