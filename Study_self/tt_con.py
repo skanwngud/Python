c406bf28bbd67fdbf31cf0e338ec9b6e854e4eea
skanwngud <skanwngud@gmail.com>
2021-05-24 17:13:12 +0900

    commit

--- Study_self/tt_con.py
+++ Study_self/tt_con.py
@@ -105,3 +105,70 @@ for t in range(epochs):
     print(f'epochs : {t + 1}')
     train(train_load, model, loss_fn, optimizer)
     test(test_load, model)
+
+
+import torch
+
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+
+train_data = datasets.MNIST(
+    root = 'data',
+    download=True,
+    train = True,
+    transform=ToTensor()
+)
+
+test_data = datasets.MNIST(
+    root = 'data',
+    download=True,
+    train=False,
+    transform=ToTensor()
+)
+
+train_load = DataLoader(
+    train_data, batch_size = 64
+)
+
+test_load = DataLoader(
+    test_data, batch_size=64
+)
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+class CNN(nn.Module):
+    def __init__(self):
+        super(CNN, self).__init__()
+
+        self.layer1 = nn.Sequential(
+            nn.Conv2d(1, 32, 1, 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)
+        )
+
+        self.layer2 = nn.Sequential(
+            nn.Conv2d(1, 64, 1, 1),
+            nn.ReLU(),
+            nn.MaxPool2d(2, 2)
+        )
+
+        self.fc = nn.Sequential(
+            nn.Linear(7*7*64, 10),
+            nn.ReLU()
+        )
+
+    def foward(self, x):
+        out = self.layer1(x)
+        out = self.layer2(out)
+        out = out.view(out.size(0), -1)
+        out = self.fc(out)
+        return out
+
+model = CNN.to(device)
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr = 1e-3)
+
+def train(dataload, model, loss_fn, optimizer):
+    
