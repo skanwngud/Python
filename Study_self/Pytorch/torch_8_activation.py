e1aa5434457acd3b14982c218a93c9af902658e7
skanwngud <skanwngud@gmail.com>
2021-05-29 17:27:20 +0900

    commit

--- Study_self/Pytorch/torch_8_activation.py
+++ Study_self/Pytorch/torch_8_activation.py
@@ -46,7 +46,7 @@ class Net(nn.Module):
         self.dropout_prob = 0.5
 
     def forward(self, x):
-        x = torch.view(-1, 28*28)
+        x = x.view(-1, 28*28)
         x = self.fc1(x)
         x = F.relu(x)
         x = F.dropout(x, p = self.dropout_prob, training=self.training)
@@ -66,3 +66,45 @@ optimizer = torch.optim.SGD(
 )
 criterion = nn.CrossEntropyLoss()
 print(model)
+
+def train(model, train_loader, optimizer, log_interval):
+    model.train()
+
+    for batch_idx, (image, label) in enumerate(train_loader):
+        image = image.to(device)
+        label = label.to(device)
+
+        optimizer.zero_grad()
+        output = model(image)
+        loss = criterion(output, label)
+        loss.backward()
+        optimizer.step()
+
+        if batch_idx % log_interval == 0:
+            print(f'Train Epoch : {epoch} [{batch_idx * len(image)}/{len(train_loader.dataset)} ({100 * batch_idx/len(train_loader):.0f})%] \tTrain Loss : {loss.item():.4f}')
+
+def evaluate(model, test_loader):
+    model.eval()
+    test_loss = 0
+    correct = 0
+
+    with torch.no_grad():
+        for image, label in test_loader:
+            image = image.to(device)
+            label = label.to(device)
+
+            output = model(image)
+            test_loss += criterion(output, label).item()
+            prediction = output.max(1, keepdim = True)[1]
+            correct += prediction.eq(label.view_as(prediction)).sum().item()
+
+        test_loss /= len(test_loader.dataset)
+        accuracy = 100 * correct / len(test_loader.dataset)
+    
+    return test_loss, accuracy
+
+for epoch in range(1, epochs + 1):
+    print(f'Epochs : {epoch}\n')
+    train(model, train_loader, optimizer, log_interval=200)
+    test_loss, accuracy = evaluate(model, test_loader)
+    print(f'\nTest Loss : {test_loss:.4f}, \tAccuracy : {accuracy}\n')
