37196cb75032c7285ab16fe01b45cb8d18a65d4f
skanwngud <skanwngud@gmail.com>
2021-06-01 12:35:34 +0900

    commit

--- Study_self/Pytorch/torch_16_transferlearning.py
+++ Study_self/Pytorch/torch_16_transferlearning.py
@@ -4,4 +4,28 @@ import torch.nn.functional as F
 
 from torchvision import datasets, transforms
 
-device
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(f'current device is {device}')
+
+train_data = datasets.CIFAR10(
+    root='data',
+    train=True,
+    download=False,
+    transform=transforms.Compose(
+        transforms.ToTensor(),
+        transforms.RandomHorizontalFlip(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    )
+)
+
+test_data = datasets.CIFAR10(
+    root='data',
+    train=False,
+    download=True,
+    transform=transforms.Compose(
+        transforms.ToTensor(),
+        transforms.RandomHorizontalFlip(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    )
+)
+
