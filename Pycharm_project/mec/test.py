9016402eb4f25f1466e2882c1cbbc5978146d7b8
skanwngud <skanwngud@gmail.com>
2021-07-29 21:39:23 +0900

    commit

---
+++ Pycharm_project/mec/test.py
@@ -0,0 +1,8 @@
+from flask import Flask, jsonify, render_template
+from subprocess import call
+from flask_socketio import SocketIO, send
+
+app = Flask(__name__)
+app.secret_key = "mysecret"
+
+socket_io = SocketIO(app)
