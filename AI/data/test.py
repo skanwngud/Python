9e13c013d7bb260c85651b5a06db5d2edda6ffac
skanwngud <skanwngud@gmail.com>
2021-11-09 21:35:35 +0900

    2021-11-09

--- AI/data/test.py
+++ AI/data/test.py
@@ -1,44 +1,17 @@
-import pandas as pd
-
-data = {
-    'A' : ['1', '2', '3', '4'],
-    'B' : ['a', 'b', 'c', 'd']
-}
-
-df = pd.DataFrame(data)
-
-print(df)
-
+import cv2 as cv
 import numpy as np
 
-x = np.array([1., 2., 3.])
-y = np.array([1., 2., 3.])
+# 기본 이미지 저장 path
+path = 'C:/Users/yjh/Downloads/'
 
-print(x, y)
+img = cv.imread(path + '4692.jpg') # 이미지 로드
+img2 = cv.cvtColor(img, cv.COLOR_BGR2GRAY) # 이미지 흑백 변환
+ret, dst = cv.threshold(img2, 200, 255, cv.THRESH_BINARY) # 이진화
 
-from tensorflow.keras.datasets import boston_housing
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
 
-X = boston_housing.load_data()
-model = Sequential()
 
-model.add(Dense(5, input_shape=(1,), activation='relu'))
-model.add(Dense(10, activation='relu'))
-model.add(Dense(4, activation='relu'))
-model.add(Dense(6, activation='relu'))
-model.add(Dense(10, activation='relu'))
-model.add(Dense(7, activation='relu'))
-model.add(Dense(2, activation='relu'))
-model.add(Dense(1))
-
-# compile, fit
-model.compile(loss='mse',metrics=['mse'])
-model.fit(x, y, epochs=1000, batch_size=1)
-
-# eval, pred
-print(model.evaluate(np.array([5]), np.array([5])))
-print(model.predict(np.array([10])))
-
-from tensorflow.keras.utils import to_categorical
-from xgboost import XGBRegressor
+# image show
+cv.imshow("img", img)
+cv.imshow("img2", img2)
+cv.imshow("img3", dst)
+cv.waitKey(0)
