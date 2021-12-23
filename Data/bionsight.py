01b0a53d92ee707916cb49f2530ed1266ae7d501
skanwngud <skanwngud@gmail.com>
2021-12-23 09:50:04 +0900

    2021-12-23 (bion)

---
+++ Data/bionsight.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Dense, Dropout, Input
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.callbacks import ReduceLROnPlateau, ModelCheckpoint, EarlyStopping
+
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import r2_score
+
+df = pd.read_csv("cmpd.csv")
+
+print(df.describe())
+
+# train, test DataFrame 생성
+train_set = pd.DataFrame(columns=['inchikey', 'smiles', 'activity'])
+test_set = pd.DataFrame(columns=['inchikey', 'smiles', 'activity'])
+
+
+for idx in range(len(df)):
+    if df.iloc[idx, 2] == 'train':
+        print(df.iloc[idx, [0, 1, 3]].values)
+        train_set.loc[idx] == df.iloc[idx, [0, 1, 3]].values
+    else:
+        test_set.loc[idx] == df.iloc[idx, [0, 1, 3]].values
