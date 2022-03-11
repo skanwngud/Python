9b11185eef83dc30e6c295fe6b7edfe6e724ba43
hyeong1145 <hyeong1145@4-lab.com>
2022-03-11 11:43:25 +0900

    commit

--- Pycharm_project/Untitled/practice.py
+++ Pycharm_project/Untitled/practice.py
@@ -1,18 +1,16 @@
-a = bin(192)
-b = bin(256)
-print(a)
-print(b)
+import time
 
-print(int(str(a), 2))
-print('a', int('0b11100000', 2))
+idx = 0
 
-print(int('0b11111000', 2))
-print(int('0b11000000', 2)) # 192
-print(pow(2, 7) + pow(2, 6))
-print(int('0b0', 2)) # 0
-print(int('0b1', 2)) # 1
-print(int('0b10', 2)) # 2
-print(int('0b11', 2)) # 3
-print(int('0b100', 2)) # 4
+def PPrint(idx, msg):
+    print(msg, 10 / idx)
+    time.sleep(2)
 
 
+while idx <= 10:
+    try:
+        PPrint(idx, "try")
+        idx += 1
+    except:
+        idx += 1
+        pass
